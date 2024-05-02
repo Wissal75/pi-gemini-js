e8850f5fe1a5cb2586676d3f8dcc24c493b8fbdc
wissal111 <wissal.miaadi@esprit.tn>
2024-05-02 15:14:26 +0100

    test

--- server.js
+++ server.js
@@ -1,35 +1,10 @@
-import express from 'express'; //library to create an api (tahki maa el server nodejs)
-import { GoogleGenerativeAI } from '@google/generative-ai'; //library mtaa gemini
-import cors from 'cors'; // library mtaa securite lel api
+import express from 'express';
+const app = express()
+const port = process.env.PORT || 4000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const app = express(); //tasna3 el api
-const PORT = 10000; //ta5tar el port mtaa el pc
-
-// Use the CORS middleware
-app.use(cors());
-
-// Endpoint to get playlists from the database
-app.get('/api/:prompt', async (req, res) => {   //7adharna fonction get b lien /api/{variable = prompt}
-
-    const prompt = req.params.prompt;
-
-    // Access your API key as an environment variable (see "Set up your API key" above)
-    const genAI = new GoogleGenerativeAI("AIzaSyC0duP5GGIP5JRj1eZOdUWWRUtLczhn2vo");
-    
-    async function run() {
-        // For text-only input, use the gemini-pro model
-        const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-      
-        const result = await model.generateContent("answer this question like you are a mediacal assisntant and dont say anything else beside the answer. the question : "+ prompt);
-        const response = await result.response;
-        const text = await response.text();
-        console.log(text);
-        res.status(200).send(text);
-    }
-    
-    run();
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
